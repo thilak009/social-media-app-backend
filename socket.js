7cf67f104f954fd5f8f44e4679c74a2639dc4d7d
thilak reddy <thilak009reddy@gmail.com>
2021-06-01 17:25:02 +0530

    heroku deploy error changes

--- socket.js
+++ socket.js
@@ -1,15 +1,15 @@
 
-exports.initializeSocket=()=>{
-    const io = require('./index')
-    io.on('connection',(socket)=>{
-        socket.on('connect chat',chatId=>{
-            socket.join(chatId)
-            console.log(chatId);
-        })
-        socket.on('new message',(message)=>{
-            console.log(message);
-        })
-        console.log("here");
-    })
-}
+// exports.initializeSocket=()=>{
+//     const io = require('./index')
+//     io.on('connection',(socket)=>{
+//         socket.on('connect chat',chatId=>{
+//             socket.join(chatId)
+//             console.log(chatId);
+//         })
+//         socket.on('new message',(message)=>{
+//             console.log(message);
+//         })
+//         console.log("here");
+//     })
+// }
 
